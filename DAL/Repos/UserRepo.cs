e45b69ec863a618e39799f2a204664b937283e6c
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-12-09 17:46:08 +0600

    UserRepo

---
+++ DAL/Repos/UserRepo.cs
@@ -0,0 +1,46 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class UserRepo : Repo, IRepo<User, int>, IAuth
+    {
+        public bool Add(User obj)
+        {
+            db.Users.Add(obj);
+            return db.SaveChanges() < 0;
+        }
+
+        public User Authenticate(int ID, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            
+        }
+
+        public List<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public bool Update(User obj)
+        {
+            var upUser = Get(obj.Id);
+            db.Entry(upUser).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
