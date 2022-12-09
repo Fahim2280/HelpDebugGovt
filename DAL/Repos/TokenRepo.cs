a490cf1c84e8a75943055aba17843b6a5839a39e
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-12-09 21:50:55 +0600

    TokenRepo

--- DAL/Repos/TokenRepo.cs
+++ DAL/Repos/TokenRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class TokenRepo
+    internal class TokenRepo : Repo, IToken<Token, string, Token>
     {
+        public Token Add(Token obj)
+        {
+            db.Tokens.Add(obj);
+            if(db.SaveChanges() > 0)
+            {
+                return obj;
+            }
+            return null;
+        }
+
+        public bool Delete(Token obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Token> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Token Get(string id)
+        {
+            return db.Tokens.FirstOrDefault(x => x.AccessToken.Equals(id));
+        }
+
+        public Token Update(Token obj)
+        {
+            var tk = Get(obj.AccessToken);
+            db.Entry(tk).CurrentValues.SetValues(obj);
+            if(db.SaveChanges() > 0)
+            {
+                return tk;
+            }
+            return null;
+        }
     }
 }
