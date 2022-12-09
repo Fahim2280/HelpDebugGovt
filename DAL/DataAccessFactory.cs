f31fb59048335fc3d3d9f99e377031e5e366c74d
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-12-09 16:55:31 +0600

    push

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Interfaces;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<User, int> UserDataAccess()
+        {
+            return new UserRepo();
+        }
+
+        public static IAuth AuthDataAccess()
+        {
+            return new UserRepo();
+        }
+    }
+}
---
+++ HelpDebugGovt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelpDebugGovt.MvcApplication" Language="C#" %>
