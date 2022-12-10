f4215dab32ba3795c6b2428198b87ff61c773f35
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-12-11 03:14:45 +0600

    Token lib change

--- BLL/Services/TokenService.cs
+++ BLL/Services/TokenService.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using BAL.DTO;
 using DAL.EF;
 using DAL;
 using System;
@@ -7,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLL.DTO;
 
 namespace BLL.Services
 {
