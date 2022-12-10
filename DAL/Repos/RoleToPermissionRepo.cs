e7344ea4a72ccee618746680d9cda2729a2bb083
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2022-12-10 21:04:28 +0600

    a

--- DAL/Repos/RoleToPermissionRepo.cs
+++ DAL/Repos/RoleToPermissionRepo.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DAL.EF;
+using DAL.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class RoleToPermissionRepos
+    internal class RoleToPermissionRepos : Repo, IRepo<RoleToPermission, String, RoleToPermission>, IAuth
     {
+        public RoleToPermission Add(RoleToPermission obj)
+        {
+            db.RoleToPermissions.Add(obj);
+            if (db.SaveChanges() > 0)
+            {
+                return obj;
+            }
+            return null;
+        }
+
+        public bool Authenticate(string Email, string pass)
+        {
+            var data = db.Users.FirstOrDefault(x => x.Email.Equals(Email) && x.Password.Equals(pass));
+            if (data != null) return true;
+            return false;
+        }
+
+        public bool Delete(string id)
+        {
+            var dbRtP= Get(id);
+            db.RoleToPermissions.Remove(dbRtP);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<RoleToPermission> Get()
+        {
+            return db.RoleToPermissions.ToList();
+        }
+
+        public RoleToPermission Get(string id)
+        {
+            return db.RoleToPermissions.Find(id);
+        }
+
+        public RoleToPermission Update(RoleToPermission obj)
+        {
+            var dbUp = db.RoleToPermissions.Find(obj.Id);
+            db.Entry(dbUp).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
     }
 }
--- DAL/Repos/SubscriptionRepo.cs
+++ DAL/Repos/SubscriptionRepo.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DAL.EF;
+using DAL.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class SubscriptionRepo
+    internal class SubscriptionRepo : Repo, IRepo<Subscription, String, Subscription'>, IAuth
     {
+        public Subscription Add(Subscription obj)
+        {
+            db.Subscriptions.Add(obj);
+            if (db.SaveChanges() > 0)
+            {
+                return obj;
+            }
+            return null;
+        }
+
+        public bool Authenticate(string Email, string pass)
+        {
+            var data = db.Users.FirstOrDefault(x => x.Email.Equals(Email) && x.Password.Equals(pass));
+            if (data != null) return true;
+            return false;
+        }
+
+        public bool Delete(string id)
+        {
+            var dbSub = Get(id);
+            db.Subscriptions.Remove(dbSub);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Subscription> Get()
+        {
+            return db.Subscriptions.ToList();
+        }
+
+        public Subscription Get(string id)
+        {
+            return db.Subscriptions.Find(id);
+        }
+
+        public Subscription Update(Subscription obj)
+        {
+            var dbUp = db.Subscriptions.Find(obj.Id);
+            db.Entry(dbUp).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
     }
 }
