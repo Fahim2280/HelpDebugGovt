ab65c93146bf746c143929b0d7be0aa220db9457
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2022-12-10 16:46:46 +0600

    Token

--- BLL/DTO/TokenDTO.cs
+++ BLL/DTO/TokenDTO.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace BAL.DTO
 {
-    internal class TokenDTO
+    public class TokenDTO
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public System.DateTime CreatedAt { get; set; }
+
+        public System.DateTime ExpiredAt { get; set; }
     }
 }
